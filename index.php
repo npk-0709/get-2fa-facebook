a4053a65a25b6c2b91af686ab87974faf536e4ec
Nguyễn Phú Khương <khuongnguyen07092005@gmail.com>
2021-08-09 12:53:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,14 @@ if(isset($_GET['key'])){
 }
 if(isset($_POST['key'])){
     $key = trim($_POST['key']);
-    $ga = new PHPGangsta_GoogleAuthenticator();
+    $ga = new PHP_GoogleAuthenticator();
     $code = $ga->getCode($key);
     $list = ["key"=>$key,"code"=>$code];
     echo json_encode($list, JSON_PRETTY_PRINT);
 }
 if(isset($_REQUEST['key'])){
     $key = trim($_REQUEST['key']);
-    $ga = new PHPGangsta_GoogleAuthenticator();
+    $ga = new PHP_GoogleAuthenticator();
     $code = $ga->getCode($key);
     $list = ["key"=>$key,"code"=>$code];
     echo json_encode($list, JSON_PRETTY_PRINT);
